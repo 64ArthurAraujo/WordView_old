39c6a088d2407dbd8959a97b7f9e411aaa3a6cdc
arthur <arthur.araujo@tutanota.com>
2022-07-23 15:05:13 -0300

    Chage states to overlay

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,15 +1,7 @@
 <script lang="ts">
-import PlusButton from "../components/PlusButton.svelte";
-import Topbar from "../components/Topbar.svelte";
-import WordmapContainer from "../components/WordmapContainer.svelte";
+
 </script>
 
-<Topbar>
-    <div class="h-full flex items-center justify-centers pl-4">
-        <h2 class="text-white text-xl select-none"><b>WordView</b></h2>
-    </div>
-</Topbar>
 
-<WordmapContainer />
 
-<PlusButton />
+
