adb8a3ad0fbe3b9a4bd74a93a0b05248a43cae3a
Arthur Araujo <arthur.araujo@tutanota.com>
2022-10-27 09:26:29 -0300

    Improve no wordmaps message animation

--- src/components/home/WordmapContainer.svelte
+++ src/components/home/WordmapContainer.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import { FrownIcon } from "svelte-feather-icons";
+  import { fly } from "svelte/transition";
   import { fetchWordmaps } from "../../actions/wordmap";
   import { wordmaps } from "../../stores/wordmap/wordmap";
   import Titlebar from "./elements/Titlebar.svelte";
@@ -13,7 +14,11 @@
 
 <div class="full center static pt-12 pb-12">
   {#if $wordmaps.length == 0}
-    <div class="center text-center column pt-72">
+    <div
+      class="center text-center column pt-72"
+      in:fly={{ x: 100, delay: 1000 }}
+      out:fly={{ x: -100, delay: 0 }}
+    >
       <FrownIcon size="48" class="invert mb-2" />
       <h4 class="ui-text align-middle">
         <b>You dont have any wordmaps yet...</b>
